3da70a60c3766460d747798cdc33a2cb4e78e837
Nathan Lisulov <nathan.lisulov@gmail.com>
2024-05-13 22:00:17 -0400

    Added changes to server.py for basic game logic and socket creation

--- Project Portfolio/Online_Console_MGame/server.py
+++ Project Portfolio/Online_Console_MGame/server.py
@@ -1,1 +1,36 @@
 import socket
+ 
+def start_server(): 
+
+    #create a server with a tcp connection
+    server_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_s.bind(("127.0.0.1", 51234))
+
+    #accepting only two connections to allow to play the game
+    server_s.listen(2)
+
+    """
+    Need to do: 
+        insert code to accept the two connections to play the game
+    """
+
+    #setting the game over condition to false, till victory condition is met.
+    game_over = False
+    while not game_over:
+        set_game_over = input("Would you like to terminate the game (enter Y or N):  ")
+        set_game_over = (set_game_over.lower())[0]
+
+        print(f"{set_game_over}\n")
+
+        if set_game_over == "y":
+            game_over = True
+    
+    #Print statement to verify end of function
+    print("End of game")
+
+
+
+
+if __name__ == "__main__":
+    start_server()
